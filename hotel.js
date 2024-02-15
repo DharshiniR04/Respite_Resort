3ae7a657d0c1fad9a53121a501da31e949ad7095
Dharshini R <114658091+DharshiniR04@users.noreply.github.com>
2024-02-15 18:38:31 +0530

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,16 @@
+function Con(){
+    location.href="./html/ContactUs.html";
+}
+
+function Abo(){
+    location.href="./html/AboutUs.html";
+}
+
+
+function Blo(){
+    location.href="./html/Blog.html";
+}
+
+function Gal(){
+    location.href="./html/Gallery.html";
+}
